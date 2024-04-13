25c54ffeffcb2a22e8431f069137bf4e2f28f11f
Monmoonluna <154536847+monmoonluna@users.noreply.github.com>
2024-04-13 03:22:16 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,12 +1,30 @@
-const express = require('express');
+//Create web server
+var express = require('express');
+var app = express();
+var fs = require('fs');
 
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+//Create server
+var server = app.listen(8080, function() {
+    console.log('Server is running...');
 });
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+//Set the view engine
+app.set('view engine', 'ejs');
+
+//Set the path for the views
+app.set('views', __dirname + '/views');
+
+//Set the path for the static files
+app.use(express.static(__dirname + '/public'));
+
+//Read the file and render the comments
+app.get('/comments', function(req, res) {
+    fs.readFile(__dirname + '/data/comments.json', 'utf8', function(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            var obj = JSON.parse(data);
+            res.render('comments', {comments: obj});
+        }
+    });
 });
